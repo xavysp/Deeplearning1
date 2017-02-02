92b04478518e153736ea978b396a9f6612cbefb4
xavysp <xaviersp18@yahoo.com>
2017-02-02 18:27:44 +0100

    checking all

--- imConvolution.py
+++ imConvolution.py
@@ -5,4 +5,33 @@
 # https://github.com/pkmital/CADL
 # the dataset which i am using on this project is CelebNet
 # Cheers xavysp
+''' import matplotlib
+matplotlib.use('GTKAgg')'''
+
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.transform import resize
+import tensorflow as tf
+
+
+# starting loading to dataset
+dirname = 'dataset'
+
+filenames = [os.path.join(dirname, fname)
+             for fname in os.listdir(dirname)]
+
+filenames =  filenames[:100]
+assert (len(filenames) == 100)
+# Now we are ready to start reading weach of 100 images in
+#  a numpy array
+
+imgs = [plt.imread(fname)[..., :3]
+        for fname in filenames]
+# we are working with images its size are 100*100*3
+# so if images are greater that mentioned the size is cropped
+#  this is not our case so y letting below code on comments
+
+imgs = np.array(imgs).astype(np.float32)
 
