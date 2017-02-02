eac2695c0cb632216562f932d5f492eeec2824c2
xavysp <xaviersp18@yahoo.com>
2017-02-02 17:14:13 +0100

    First check

---
+++ imConvolution.py
@@ -0,0 +1,8 @@
+## Hello to everybody, I am starting to learn tensorFlow.
+# this code is not mine, I am using only for learning.
+# If you want information or wanna get the original code
+# please feel free to visit this site
+# https://github.com/pkmital/CADL
+# the dataset which i am using on this project is CelebNet
+# Cheers xavysp
+
